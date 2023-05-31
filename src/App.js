c822a09ae67db44f17f43f946f88a3558db414a4
Adam Webb <adam.webb@mrjaffesclass.com>
2023-05-31 12:40:02 -0700

    Switch to react because I can't figure out next js and my deadline is coming up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import openWindow from "./open_window.png"
+
+function App() {
+  return (
+    <div className="App">
+      <image src={openWindow} alt={"Nope, u thought"}/>
+    </div>
+  );
+}
+
+export default App;
