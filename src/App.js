fd98992f0a0c094fb0e0167546b1508ca0bff1be
Adam Webb <adam.webb@mrjaffesclass.com>
2023-06-02 12:45:51 -0700

    Layered fetch?

--- src/App.js
+++ src/App.js
@@ -7,27 +7,49 @@ import Water from './Sensors/Water';
 
 function App() {
 
-  const [authKey, setAuthKey] = useState(() => getInitialAuthKey)
+  const [authKey, setAuthKey] = useState(0)
+  getInitialAuthKey()
   const [sensorData, setSensorData] = useState(() => getSensorData(authKey))
 
   function getInitialAuthKey(){
-    fetch("https://z6my0pr9u3.execute-api.us-west-1.amazonaws.com/prod/auth",
-          {
-            method: "POST",
-            headers: {
-              "Accept": "application/json",
-              "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-              username: "123456",
-              password: "password"
-            })
-          })
-    .then(resp=> resp.text())
-    .then(key=> {console.log(key);setAuthKey(key)})
+    let authKey
+    
   }
 
   function getSensorData(authKey){
+    // if (authKey == 0){
+    //   fetch("https://z6my0pr9u3.execute-api.us-west-1.amazonaws.com/prod/auth",
+    //   {
+    //     method: "POST",
+    //     headers: {
+    //       "Accept": "application/json",
+    //       "Content-Type": "application/json"
+    //     },
+    //     body: JSON.stringify({
+    //       username: "123456",
+    //       password: "password"
+    //     })
+    //   })
+    //   .then(resp=> resp.text())
+    //   .then(key=> {
+    //     let token = JSON.parse(key)["token"]
+    //     console.log(token)
+    //     fetch("https://z6my0pr9u3.execute-api.us-west-1.amazonaws.com/prod/get",
+    //       {
+    //         method: "POST",
+    //         headers: {
+    //           "Accept": "application/json",
+    //           "Content-Type": "application/json"
+    //         },
+    //         body: JSON.stringify({
+    //           token: {token},
+    //         })
+    //       })
+    //     .then(resp=> resp)
+    //     .then(data=> console.log(data))
+        
+    //   })
+  }
     fetch("https://z6my0pr9u3.execute-api.us-west-1.amazonaws.com/prod/get",
           {
             method: "POST",
@@ -36,19 +58,24 @@ function App() {
               "Content-Type": "application/json"
             },
             body: JSON.stringify({
-              token: {authKey},
-              
+              token: "SHZLMKJNMT",
             })
           })
-    .then(resp=> resp.text())
-    .then(data=> {console.log(data);setAuthKey(data.token)})
+        .then(resp=> resp)
+        .then(data=> console.log(data))
+    
+  
+
+  function refreshSensorData(){
+    setSensorData(getSensorData(authKey))
+    return sensorData
   }
 
   
 
   return (
-    <div className="App" onLoad={getInitialAuthKey}>
-      <button onClick={getSensorData(authKey)}>Click me</button>
+    <div className="App">
+      <button onClick={refreshSensorData}>Click me</button>
     </div>
   );
 }
